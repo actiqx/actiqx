ae48c548809605fd0ce018aeef489c25803b6f68
unknown <ppatra@ws-inblr696.ad.agi>
2017-07-03 18:55:17 +0530

    Initial

---
+++ app/js/app.run.js
@@ -0,0 +1,26 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('app')
+        .run(runApp)
+
+    runApp.$inject = ['$ionicPlatform'];
+
+    function runApp($ionicPlatform){
+        $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+    }
+
+}());
