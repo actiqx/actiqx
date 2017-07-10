8b573b1f23c15a0072288925761c1b83e36d765f
Priya4110 <priyatoshpatra@gmail.com>
2017-07-11 00:49:22 +0530

    added bottom nav

---
+++ app/js/component/services/dashboard.service.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.dashboard')
+    .service('dashboardService', dashboardService)
+
+  dashboardService.$inject = ['$http', 'server', 'exception', 'logger'];
+  function dashboardService($http, server, exception, logger) {
+    var servicecalls = {
+      getdashboarddata: getdashboarddata
+    };
+    return servicecalls;
+
+    function getdashboarddata() {
+
+     return $http.get(`${server.host}:${server.port}${server.dashboard}`)
+        .then(success)
+        .catch(fail);
+
+    }
+
+    function success(response) {
+  logger.success(response.data);
+      return response.data;
+    }
+
+    function fail(e) {
+      return exception.catcher('XHR Failed for getPeople')(e);
+    }
+
+  }
+
+}());
